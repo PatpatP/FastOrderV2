47a9081bf8f6ce1fcea918f74da06a542cd795f2
T-Rox <roubrerie@gmail.com>
2016-07-10 10:44:17 +0200

    Adress + tests Adress et Product

--- tests/com/fastorder/manager/tests/AddressManagerTests.java
+++ tests/com/fastorder/manager/tests/AddressManagerTests.java
@@ -8,12 +8,13 @@ import org.junit.Test;
 import com.fastorder.manager.impl.AddressManagerImpl;
 import com.fastorder.model.Address;
 import com.fastorder.utils.UtilsBdd;
+import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.Statement;
 
 public class AddressManagerTests {
 	
 	private AddressManagerImpl addressManager;
-	private Statement statement;
+	private Connection connection;
 	
 	private String street;
 	private String number;
@@ -23,8 +24,8 @@ public class AddressManagerTests {
 	
 	@Before
 	public void setUp(){
-		statement = UtilsBdd.connectBDD();
-		addressManager = new AddressManagerImpl(statement);
+		connection = UtilsBdd.connectBDD();
+		addressManager = new AddressManagerImpl(connection);
 		
 		street = "Street Test";
 		number= "30";
@@ -36,16 +37,16 @@ public class AddressManagerTests {
 	@Test
 	public void createAddressTest(){
 		boolean isCreated = addressManager.createAddress(street, number, zipCode, city, country);
-		assertTrue("CrÃ©ation de l'adresse", isCreated);
+		assertTrue("Création de l'adresse", isCreated);
 		
 		int idAddress = addressManager.getAddressId(street, number, zipCode, city, country);
 		Address address = addressManager.getAddresssById(idAddress);
 		
-		assertEquals("VÃ©rification du nom de rue", street, address.getStreet());
-		assertEquals("Verification du numÃ©ro de rue", number, address.getNumber());
-		assertEquals("VÃ©rification du code postal", zipCode, address.getZipCode());
-		assertEquals("VÃ©rification de la ville", city, address.getCity());
-		assertEquals("VÃ©rification du pays", country, address.getCountry());
+		assertEquals("Vérification du nom de rue", street, address.getStreet());
+		assertEquals("Verification du numéro de rue", number, address.getNumber());
+		assertEquals("Vérification du code postal", zipCode, address.getZipCode());
+		assertEquals("Vérification de la ville", city, address.getCity());
+		assertEquals("Vérification du pays", country, address.getCountry());
 		
 		boolean isDeleted = addressManager.deleteAddress(idAddress);
 		assertTrue("Suppression de l'adresse", isDeleted);
@@ -54,31 +55,31 @@ public class AddressManagerTests {
 	@Test
 	public void updateAddressTest(){
 		boolean isCreated = addressManager.createAddress(street, number, zipCode, city, country);
-		assertTrue("CrÃ©ation de l'adresse", isCreated);
+		assertTrue("Création de l'adresse", isCreated);
 		
 		int idAddress = addressManager.getAddressId(street, number, zipCode, city, country);
 		Address address = addressManager.getAddresssById(idAddress);
 		
-		assertEquals("VÃ©rification du nom de rue", street, address.getStreet());
-		assertEquals("Verification du numÃ©ro de rue", number, address.getNumber());
-		assertEquals("VÃ©rification du code postal", zipCode, address.getZipCode());
-		assertEquals("VÃ©rification de la ville", city, address.getCity());
-		assertEquals("VÃ©rification du pays", country, address.getCountry());
+		assertEquals("Vérification du nom de rue", street, address.getStreet());
+		assertEquals("Verification du numéro de rue", number, address.getNumber());
+		assertEquals("Vérification du code postal", zipCode, address.getZipCode());
+		assertEquals("Vérification de la ville", city, address.getCity());
+		assertEquals("Vérification du pays", country, address.getCountry());
 		
 		String newStreetName = "New Street Test";
 		String newNumber = "42";
 		String newCity = "New City Test";
 		
 		boolean isUpdated = addressManager.updateAddress(idAddress, newStreetName, newNumber, zipCode, newCity, country);
-		assertTrue("Mise Ã  jour de l'address", isUpdated);
+		assertTrue("Mise à jour de l'address", isUpdated);
 		
 		Address newAddress = addressManager.getAddresssById(idAddress);
 		
-		assertEquals("VÃ©rification du nom de rue", newStreetName, newAddress.getStreet());
-		assertEquals("Verification du numÃ©ro de rue", newNumber, newAddress.getNumber());
-		assertEquals("VÃ©rification du code postal", zipCode, newAddress.getZipCode());
-		assertEquals("VÃ©rification de la ville", newCity, newAddress.getCity());
-		assertEquals("VÃ©rification du pays", country, newAddress.getCountry());
+		assertEquals("Vérification du nom de rue", newStreetName, newAddress.getStreet());
+		assertEquals("Verification du numéro de rue", newNumber, newAddress.getNumber());
+		assertEquals("Vérification du code postal", zipCode, newAddress.getZipCode());
+		assertEquals("Vérification de la ville", newCity, newAddress.getCity());
+		assertEquals("Vérification du pays", country, newAddress.getCountry());
 		
 		boolean isDeleted = addressManager.deleteAddress(idAddress);
 		assertTrue("Suppression de l'adresse", isDeleted);
